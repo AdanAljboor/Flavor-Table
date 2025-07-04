dfacad103b6931f62abad3d110b404e4330bbaec
AdanAljboor <Adan.Aljboorr@gmail.com>
2025-07-04 05:19:57 +0300

    Final version for Part 2 - PostgreSQL integration and full CRUD

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
 });
 
-pool.connect()
-    .then(() => console.log('Connected to database'))
-    .catch(err => console.error('Database connection error', err));
-
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
